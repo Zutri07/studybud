cd068edcf74c02eb87f9092d75d3b7dfcb3135a7
Zutri <al386130@uji.es>
2022-02-11 13:57:07 +0100

    migracion de participantes hecha

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from django.contrib.auth.models import User
 from .forms import RoomForm
 from django.contrib import messages
@@ -68,8 +68,17 @@ def home(request):
 
 def room(request, pk):
     room= Room.objects.get(id = pk)
-    messeges = room.message_set.all()
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method=='POST':
+        message= Message.objects.create(
+            user=request.user,
+            room=room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+        
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url="login")
