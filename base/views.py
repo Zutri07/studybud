cf47b6a4f523b7245ae212b78c7f8730fb3d3859
Zutri <al386130@uji.es>
2022-02-11 13:11:39 +0100

    Hasta la creacion de los comments

--- base/views.py
+++ base/views.py
@@ -68,6 +68,7 @@ def home(request):
 
 def room(request, pk):
     room= Room.objects.get(id = pk)
+    messeges = room.message_set.all()
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
